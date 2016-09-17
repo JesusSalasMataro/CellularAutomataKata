7c437597edad8491afc92df33e2c3da8b8e5d749
JesusSalasMataro <jesus459@hotmail.es>
2016-09-18 00:05:36 +0200

    Initial code

---
+++ CellularAutomataKata/CellularAutomata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CellularAutomataKata
+{
+    public class CellularAutomata
+    {
+
+        public List<string> Run(string inputString, int steps)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTests/Given_AnInputString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CellularAutomataKata;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Given_AnInputString
+    {
+        [TestMethod]
+        public void When_InputStringIsEmpty_Then_ReturnEmptyOutput()
+        {
+            // ARRANGE
+            CellularAutomata cellularAutomata = new CellularAutomata();
+            string inputString = "";
+            int steps = 5;
+            List<string> expectedOutput = new List<string>();
+
+            // ACT
+            List<string> actualOutput = cellularAutomata.Run(inputString, steps);
+
+            // ASSERT
+            CollectionAssert.AreEqual(expectedOutput, actualOutput);
+        }
+    }
+}
