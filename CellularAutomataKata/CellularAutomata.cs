236b2e6298edd46d3529309c8d51f708f2e4cad9
JesusSalasMataro <jesus459@hotmail.es>
2016-09-18 00:50:00 +0200

    Green tests

--- CellularAutomataKata/CellularAutomata.cs
+++ CellularAutomataKata/CellularAutomata.cs
@@ -8,10 +8,85 @@ namespace CellularAutomataKata
 {
     public class CellularAutomata
     {
+        private Dictionary<string, char> _rule90;
+
+        public CellularAutomata()
+        {
+            InitRule90();
+        }
 
         public List<string> Run(string inputString, int steps)
         {
-            throw new NotImplementedException();
+            List<string> output = new List<string>();
+            int inputStringLength = inputString.Length;
+            string valueToEvaluate, resultValue;
+
+            if (inputStringLength == 0)
+            {
+                throw new ArgumentNullException();
+            }
+
+            output.Add(inputString);
+
+            for (int i = 0; i < steps; i++)
+            {
+                resultValue = "";
+
+                for (int ii = 0; ii < inputStringLength; ii++)
+                {
+                    valueToEvaluate = "";
+
+                    if (ii == 0)
+                    {
+                        valueToEvaluate = "0";
+                    }
+                    else
+                    {
+                        valueToEvaluate += inputString[ii - 1];
+                    }
+
+                    valueToEvaluate += inputString[ii];
+
+                    if (ii == inputStringLength - 1)
+                    {
+                        valueToEvaluate += "0";
+                    }
+                    else
+                    {
+                        valueToEvaluate += inputString[ii + 1];
+                    }
+
+                    resultValue += GetValue(valueToEvaluate).ToString();
+                }
+
+                output.Add(resultValue);
+                inputString = resultValue;
+            }
+
+            return output;
         }
+
+
+        #region Private methods
+        
+        private void InitRule90()
+        {
+            _rule90 = new Dictionary<string, char>();
+            _rule90.Add("111", '0');
+            _rule90.Add("110", '1');
+            _rule90.Add("101", '0');
+            _rule90.Add("100", '1');
+            _rule90.Add("011", '1');
+            _rule90.Add("010", '0');
+            _rule90.Add("001", '1');
+            _rule90.Add("000", '0');
+        }
+
+        private char GetValue(string valueToEvaluate)
+        {
+            return _rule90[valueToEvaluate];
+        }
+
+        #endregion
     }
 }
--- UnitTests/Given_AnInputString.cs
+++ UnitTests/Given_AnInputString.cs
@@ -8,14 +8,114 @@ namespace UnitTests
     [TestClass]
     public class Given_AnInputString
     {
+        [ExpectedException(typeof(ArgumentNullException))]
         [TestMethod]
-        public void When_InputStringIsEmpty_Then_ReturnEmptyOutput()
+        public void When_InputStringIsEmpty_Then_ReturnInputString()
         {
             // ARRANGE
             CellularAutomata cellularAutomata = new CellularAutomata();
             string inputString = "";
+            int steps = 1;
+
+            // ACT
+            List<string> actualOutput = cellularAutomata.Run(inputString, steps);
+
+            // ASSERT
+
+        }
+
+        [TestMethod]
+        public void When_InputStringLengthIs1_Then_ReturnCorrectOutput()
+        {
+            // ARRANGE
+            CellularAutomata cellularAutomata = new CellularAutomata();
+            string inputString = "1";
+            int steps = 1;
+            List<string> expectedOutput = new List<string> {
+                "1",
+                "0"
+            };
+
+            // ACT
+            List<string> actualOutput = cellularAutomata.Run(inputString, steps);
+
+            // ASSERT
+            CollectionAssert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod]
+        public void When_InputStringLengthIs2_Then_ReturnCorrectOutput()
+        {
+            // ARRANGE
+            CellularAutomata cellularAutomata = new CellularAutomata();
+            string inputString = "11";
+            int steps = 1;
+            List<string> expectedOutput = new List<string> {
+                "11",
+                "11"
+            };
+
+            // ACT
+            List<string> actualOutput = cellularAutomata.Run(inputString, steps);
+
+            // ASSERT
+            CollectionAssert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod]
+        public void When_InputStringIsNotEmptyAndOneStep_Then_ReturnCorrectOutput()
+        {
+            // ARRANGE
+            CellularAutomata cellularAutomata = new CellularAutomata();
+            string inputString = "1101010";
+            int steps = 1;
+            List<string> expectedOutput = new List<string> {
+                "1101010",
+                "1100001"
+            };
+
+            // ACT
+            List<string> actualOutput = cellularAutomata.Run(inputString, steps);
+
+            // ASSERT
+            CollectionAssert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod]
+        public void When_InputStringIsNotEmptyAndTwoSteps_Then_ReturnCorrectOutput()
+        {
+            // ARRANGE
+            CellularAutomata cellularAutomata = new CellularAutomata();
+            string inputString = "1101010";
+            int steps = 2;
+            List<string> expectedOutput = new List<string> {
+                "1101010",
+                "1100001",
+                "1110010"
+            };
+
+            // ACT
+            List<string> actualOutput = cellularAutomata.Run(inputString, steps);
+
+            // ASSERT
+            CollectionAssert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod]
+        public void When_InputStringIsNotEmptyAndSeveralSteps_Then_ReturnCorrectOutput()
+        {
+            // ARRANGE
+            CellularAutomata cellularAutomata = new CellularAutomata();
+            string inputString = "1101010";
             int steps = 5;
-            List<string> expectedOutput = new List<string>();
+            List<string> expectedOutput = new List<string> {
+                "1101010",
+                "1100001",
+                "1110010",
+                "1011101",
+                "0010100",
+                "0100010"
+            };
 
             // ACT
             List<string> actualOutput = cellularAutomata.Run(inputString, steps);
